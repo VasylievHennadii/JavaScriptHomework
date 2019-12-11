36015462570369b0e4ede00ef9ab5b4632fb9850
VasylievHennadii <vga.kha@gmail.com>
2019-12-11 14:08:06 +0200

    add file readme

---
+++ hw2/hw2.js
@@ -0,0 +1,17 @@
+
+
+
+
+// task-9
+// Даны объект и функция:
+//     let sizes = { width: 5, height: 10 },
+//     getSquare = function () { return this.width * this.height };
+// Не изменяя функцию или объект, получить результат функции
+// *getSquare* для объекта sizes
+
+let sizes = { width: 5, height: 10 };
+getSquare = function () { return this.width * this.height };
+(getSquare)(sizes);
+console.log(sizes);
+
+
